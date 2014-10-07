77771eb7e2abc41b2ab53005e8dac1af292e639b
Matt Ronchetti <ronche@gmail.com>
2014-10-07 13:06:32 -0700

    Initial commit
    Removed tools I don’t typically use for a more “baseline” starter
    project

--- gulp/config.js
+++ gulp/config.js
@@ -6,17 +6,13 @@ module.exports = {
     server: {
       // We're serving the src folder as well
       // for sass sourcemap linking
-      baseDir: [dest, src]
+      baseDir: [dest]
     },
     files: [
       dest + "/**",
       // Exclude Map files
       "!" + dest + "/**.map"
     ]
-  },
-  sass: {
-    src: src + "/sass/*.{sass, scss}",
-    dest: dest
   },
   images: {
     src: src + "/images/**",
@@ -30,17 +26,13 @@ module.exports = {
     // Enable source maps
     debug: true,
     // Additional file extentions to make optional
-    extensions: ['.coffee', '.hbs'],
+    //extensions: ['.coffee', '.hbs'],
     // A separate bundle will be generated for each
     // bundle config in the list below
     bundleConfigs: [{
-      entries: './src/javascript/app.coffee',
+      entries: './src/javascript/app.js',
       dest: dest,
       outputName: 'app.js'
-    }, {
-      entries: './src/javascript/head.coffee',
-      dest: dest,
-      outputName: 'head.js'
     }]
   }
 };
